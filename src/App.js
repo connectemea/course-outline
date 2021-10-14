7159be3c7d0fdf6ec835b6ff880ebc78750d13ab
afeef <afeef@sanoft.com>
2021-10-14 23:06:24 +0530

    hashrouter converted to router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useState } from "react";
 import "./App.css";
 import { Container } from "@mui/material";
